4545d0c3a12a84c42a7335dd7aeef041e2a5c71b
Claire <claire.hw.cho@gmail.com>
2023-01-30 14:21:06 -0800

    initial commit

---
+++ routes/notes.js
@@ -0,0 +1,11 @@
+const notesRouter = require('express').Router();
+
+notesRouter.get('/', (req, res) => {
+    // code here
+});
+
+notesRouter.post('/', (req, res) => {
+    // code here
+});
+
+module.exports = notesRouter;
